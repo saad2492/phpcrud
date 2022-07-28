42f4f3b4e7c376ef26ffcc8a4a563a01fbe67403
saad <zubaer35-2492@diu.edu.bd>
2022-07-28 15:22:29 +0600

    changed main.php

--- main.php
+++ main.php
@@ -98,8 +98,3 @@ include "class.php";
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-qwdqwdawd
-
-efsefse
